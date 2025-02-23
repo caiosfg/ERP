f3c5604bc90908dada85b03361be2a6c18682563
caiosfg <caio.sfg@gmail.com>
2025-02-23 16:45:09 -0300

    feat: entity user

--- api/src/users/entities/user.entity.ts
+++ api/src/users/entities/user.entity.ts
@@ -1,1 +1,30 @@
-export class User {}
+import { IsEmail } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('user')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @IsEmail()
+  email: string;
+
+  @Column({ length: 255 })
+  passwordHash: string;
+
+  @Column({ length: 255 })
+  name: string;
+
+  @CreateDateColumn()
+  createdAt?: Date;
+
+  @UpdateDateColumn()
+  updatedAt?: Date;
+}
