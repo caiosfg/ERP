dc0d98575c3047753ec621f558aa3d0898c69d76
caiosfg <caio.sfg@gmail.com>
2025-02-22 23:46:33 -0300

    feat:global pipes, validation, class

--- api/src/products/entities/product.entity.ts
+++ api/src/products/entities/product.entity.ts
@@ -1,8 +1,16 @@
+import { IsDate, IsNotEmpty, IsNumber, MaxLength } from "class-validator";
+
 export class Product {
+  @IsNumber()
   id: number;
+  @IsNotEmpty()
+  @MaxLength(255)
   description: string;
+  @MaxLength(255)
   price: string;
-  stock: string;
+  @IsNumber()
+  stock: number;
   image: string;
+  @IsDate()
   date: Date;
 }
