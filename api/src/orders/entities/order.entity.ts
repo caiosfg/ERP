c0a9a73c0c49a68813ca34fbc84ac0e7ab692a12
caiosfg <caio.sfg@gmail.com>
2025-02-23 17:10:37 -0300

    feat: entity orders

--- api/src/orders/entities/order.entity.ts
+++ api/src/orders/entities/order.entity.ts
@@ -1,1 +1,34 @@
-export class Order {}
+import { Person } from 'src/persons/entities/person.entity';
+import { Product } from 'src/products/entities/product.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('order')
+export class Order {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Person, (person) => person.id)
+  @JoinColumn({ name: 'person_id' })
+  person: Person;
+
+  @ManyToOne(() => Product, (product) => product.id)
+  @JoinColumn({ name: 'product_id' })
+  product: Person;
+
+  @Column()
+  amount: number;
+
+  @CreateDateColumn()
+  createdAt?: Date;
+
+  @UpdateDateColumn()
+  updatedAt?: Date;
+}
