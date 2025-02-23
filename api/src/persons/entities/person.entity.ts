235c8e0999ed5207c90ac95cf1c438f7d49e0183
caiosfg <caio.sfg@gmail.com>
2025-02-23 17:01:31 -0300

    feat: entity person

--- api/src/persons/entities/person.entity.ts
+++ api/src/persons/entities/person.entity.ts
@@ -1,1 +1,37 @@
-export class Person {}
+import { IsEmail } from 'class-validator';
+import { User } from 'src/users/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('person')
+export class Person {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 255 })
+  name: string;
+
+  @Column()
+  cnpj: number;
+
+  @Column()
+  @IsEmail()
+  email: string;
+
+  @ManyToOne(() => User, (user) => user.id)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @CreateDateColumn()
+  createdAt?: Date;
+
+  @UpdateDateColumn()
+  updatedAt?: Date;
+}
