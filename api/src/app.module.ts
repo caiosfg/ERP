29591ae7fd919db4d2fbe94e335431f617094f1e
caiosfg <caio.sfg@gmail.com>
2025-02-23 16:07:53 -0300

    feat: nest g persons

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PersonsModule } from './persons/persons.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       synchronize: true,
     }),
     ProductsModule,
+    PersonsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
