51b32435aa37998434f0a8c823e790d8dfafa2c9
caiosfg <caio.sfg@gmail.com>
2025-02-23 15:28:52 -0300

    feat: crud of products

--- api/src/products/products.controller.ts
+++ api/src/products/products.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  findAll(@Query() pagination: any) {
+    const { limit = 10, offset = 0 } = pagination;
+    return this.productsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.productsService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productsService.create(createProductDto);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productsService.update(id, updateProductDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.productsService.delete(id);
+  }
+}
--- api/src/products/products.service.ts
+++ api/src/products/products.service.ts
@@ -1,8 +1,66 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateProductDto } from './dto/create-product.dto';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductsService {
-    test() {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
 
-    }
+  async findAll() {
+    const products = await this.productRepository.find();
+    return products;
+  }
+
+  async findOne(id: number) {
+    const product = await this.productRepository.findOneBy({
+      id,
+    });
+
+    if (!product) return this.throwNotFoundError();
+
+    return product;
+  }
+
+  async create(createProductDto: CreateProductDto) {
+    const newProduct = {
+      ...createProductDto,
+      data: new Date(),
+    };
+
+    const product = this.productRepository.create(newProduct);
+    return this.productRepository.save(product);
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const product = await this.productRepository.preload({
+      id,
+      ...updateProductDto,
+    });
+
+    if (!product) return this.throwNotFoundError();
+
+    await this.productRepository.save(product);
+
+    return product;
+  }
+
+  async delete(id: number) {
+    const product = await this.productRepository.findOneBy({
+      id,
+    });
+
+    if (!product) return this.throwNotFoundError();
+
+    return this.productRepository.remove(product);
+  }
+
+  throwNotFoundError() {
+    throw new NotFoundException('Produto não encontrado');
+  }
 }
