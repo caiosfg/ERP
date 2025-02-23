92304efa63c78ae1ac8b83c2434d7aa6fd665a45
caiosfg <caio.sfg@gmail.com>
2025-02-23 19:59:31 -0300

    feat: crud for orders

--- api/src/orders/dto/create-order.dto.ts
+++ api/src/orders/dto/create-order.dto.ts
@@ -1,1 +1,18 @@
-export class CreateOrderDto {}
+import { IsNotEmpty, IsNumber, MinLength } from 'class-validator';
+
+export class CreateOrderDto {
+  @IsNumber()
+  @IsNotEmpty()
+  @MinLength(4)
+  person_id: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @MinLength(4)
+  product_id: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @MinLength(4)
+  amount: number;
+}
--- api/src/orders/orders.service.ts
+++ api/src/orders/orders.service.ts
@@ -1,26 +1,80 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class OrdersService {
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+  constructor(
+    @InjectRepository(Order)
+    private readonly orderRepository: Repository<Order>,
+  ) {}
+
+  async create(createOrderDto: CreateOrderDto) {
+    try {
+      const orderDto = {
+        person_id: createOrderDto.person_id,
+        product_id: createOrderDto.product_id,
+        amount: createOrderDto.amount,
+      };
+
+      const order = this.orderRepository.create(orderDto);
+      await this.orderRepository.save(order);
+
+      return order;
+    } catch (error) {
+      throw new NotFoundException(`message: ${error}`);
+    }
   }
 
-  findAll() {
-    return `This action returns all orders`;
+  async findAll() {
+    const orders = await this.orderRepository.find({
+      order: {
+        id: 'DESC',
+      },
+    });
+
+    return orders;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
+  async findOne(id: number) {
+    const order = await this.orderRepository.findOneBy({
+      id,
+    });
+
+    if (!order) throw new NotFoundException(`Order ${id} not found !`);
+
+    return order;
   }
 
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
+  async update(id: number, updateOrderDto: UpdateOrderDto) {
+    const orderDto = {
+      person_id: updateOrderDto?.person_id,
+      product_id: updateOrderDto?.product_id,
+      amount: updateOrderDto?.amount,
+    };
+
+    const order = await this.orderRepository.preload({
+      id,
+      ...orderDto,
+    });
+
+    if (!order) throw new NotFoundException(`Order ${id} not found !`);
+
+    await this.orderRepository.save(order);
+
+    return order;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} order`;
+  async remove(id: number) {
+    const order = await this.orderRepository.findOneBy({
+      id,
+    });
+
+    if (!order) throw new NotFoundException(`order ${id} not found !`);
+
+    return this.orderRepository.remove(order);
   }
 }
